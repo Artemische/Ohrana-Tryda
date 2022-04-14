aaac3580c96864fe8690658163d84b9460a04043
Siarhei Shnitko <siarhei.shnitko@leverx.com>
2022-04-14 18:03:38 +0300

    add login popup

--- uimodule/webapp/controller/ListReport.controller.js
+++ uimodule/webapp/controller/ListReport.controller.js
@@ -1,7 +1,39 @@
-sap.ui.define(["bntu/ohranaTryda/controller/BaseController"], function (Controller) {
+sap.ui.define([
+    "bntu/ohranaTryda/controller/BaseController",
+    "sap/ui/core/Fragment",
+], function (Controller, Fragment) {
     "use strict";
 
     return Controller.extend("bntu.ohranaTryda.controller.ListReport", {
         
+        onInit() {
+            this.openLoginDialog();
+        },
+
+        openLoginDialog() {
+            var oView = this.getView();
+            if (!this.oLoginDialog) {
+                this.oLoginDialog = Fragment.load({
+                    id: oView.getId(),
+                    name: "bntu.ohranaTryda.view.fragments.Login",
+                    controller: this
+                }).then(oLoginDialog => {
+                    oView.addDependent(oLoginDialog);
+                    return oLoginDialog;
+                });
+            }
+            this.oLoginDialog.then(oLoginDialog => {
+                oLoginDialog.open();
+            });
+        },
+
+        onCancelPress() {
+            if (this.oLoginDialog) {
+                this.oLoginDialog.then(oLoginDialog => {
+                    oLoginDialog.close().destroy();
+                    this.oLoginDialog = null;
+                });
+            }
+        },
     });
 });
